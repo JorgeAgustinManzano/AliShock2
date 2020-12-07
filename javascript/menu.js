d8eea78d66601d800cc537f27a88fbdbbc63863b
JorgeAgustinManzano <titoestito@hotmail.com>
2020-12-06 23:57:22 -0300

    agregue galeria imagenes y galeria videos

--- javascript/menu.js
+++ javascript/menu.js
@@ -73,9 +73,10 @@ let encabezado =
                             Galeria
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" data-toggle="modal" data-target="#modalVideos" href="#">Videos</a>
-                            <a class="dropdown-item" data-toggle="modal" data-target="#modalImagenes" href="#">Fotos</a>
-                            <a class="dropdown-item" href="#">Otros</a>
+                            <a class="dropdown-item" data-toggle="modal" data-target="#modalGVideos" href="#">Galeria Videos</a>
+                            <a class="dropdown-item" data-toggle="modal" data-target="#modalGImagenes" href="#">Galeria Fotos</a>
+                            <a class="dropdown-item" data-toggle="modal" data-target="#modalVideos" href="#">Carusel Videos</a>
+                            <a class="dropdown-item" data-toggle="modal" data-target="#modalImagenes" href="#">Carusel Fotos</a>       
                         </div>
                     </li>
                 </ul>
@@ -101,20 +102,20 @@ let pieDePagina =
         <div class="col-md-12 py-1">
             <div class="mb-2 flex-center">
                 <a class="fb-ic">
-                    <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    <i class="fab fa-facebook-f fa-lg white-text mr-2"></i>
                 </a>
                 <a class="tw-ic">
-                    <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    <i class="fab fa-twitter fa-lg white-text"></i>
                 </a>
                 <a class="ins-ic">
-                    <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    <i class="fab fa-instagram fa-lg white-text ml-2"></i>
                 </a>
             </div>
         </div>
     </div>
 </div>
 <div class="footer-copyright text-center py-1">© 2020 Derechos Reservados:
-    <a href="https://mdbootstrap.com/"> AliShock</a>
+    <a href=#> AliShock</a>
 </div>
 </footer>`
 
@@ -182,9 +183,9 @@ aria-labelledby="exampleModalLabel" aria-hidden="true">
                         Nuestra misión es obtener la imagen deseada por los clientes, fortificando su
                         autoestima.
                         Brindamos atención y servicio personalizado que transciende lo estético y crea
-                        relaciones de impacto conlosclientes.<br><br>
+                        relaciones de impacto con los clientes.<br><br>
                         Para nosotros lo más importante son nuestras clientas/es.
-                        Nos especializamos en Alisados, shock, botox, otros.. Trabajamos con los mejores
+                        Nos especializamos en Alisados, Shock, Botox y otros.. Trabajamos con los mejores
                         productos del mercado.
                         Damos garantía y soporte pos tratamiento por todos nuestros trabajos.
                     </p>
@@ -300,10 +301,94 @@ modalCarruselVideos += `
         </div>
     </div>
     `
+
+// Galeria de Imagenes
+
+let modalGaleriaImagenes =
+    `
+    <div class="modal fade bd-example-modal-lg" id="modalGImagenes" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <!--Content-->
+            <div class="modal-content">
+                <!--Header-->
+                <div class="modal-header">
+                    <p class="heading">AliShock - Galeria de Imagenes</p>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
+                </div>
+
+                <!--Body-->
+                <div class="modal-body bodyGal">
+                    <div class="container page-top">
+                        <div class="row" id="gal_Imagenes">`                                                  
+for (let i = 0; i < imgC.length; i++) {
+    modalGaleriaImagenes += `<div class="col-lg-3 col-md-4 col-xs-6 thumb">                           
+                                    <img id="imagenG${i}" src="${imgC[i]}" class="zoom img-fluid "  alt="" @click="abrirImagenGal(${i})" data-toggle="modal" data-target="#myModal">
+                            </div>`
+}
+modalGaleriaImagenes += `                                
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+
+let modalGal = `
+    <div id="myModal" class="modal modalG" tabindex="0" role="dialog">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target="#modalGImagenes">
+            <span aria-hidden="true" class="white-text">&times;</span>
+        </button>
+        <img class="modal-content modal-contentG" id="imgen01">
+        <br>
+        <!--<div id="caption"></div>-->
+    </div>
+`
+let modalGaleriaVideos =
+    `
+    <div class="modal fade bd-example-modal-lg" id="modalGVideos" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <!--Content-->
+            <div class="modal-content">
+                <!--Header-->
+                <div class="modal-header">
+                    <p class="heading">AliShock - Galeria de Videos</p>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
+                </div>
+
+                <!--Body-->
+                <div class="modal-body bodyGal">
+                    <div class="container page-top">
+                        <div class="row" id="gal_Imagenes">`                                                  
+for (let i = 0; i < videosC.length; i++) {
+    modalGaleriaVideos += ` <div class="col-lg-4 col-md-6 col-xs-12 thumb">                           
+                                <div class="zoom img-fluid mx-auto d-block altoyancho">${videosC[i]} alt="slide ${i}"></iframe></div>
+                            </div>`
+}
+modalGaleriaVideos += `                                
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+
 document.getElementById('idHeader').innerHTML = encabezado;
 document.getElementById('modalServ').innerHTML = modalServicios;
 document.getElementById('modalNosotros').innerHTML = modalNosotros;
 document.getElementById('modalCarrusel').innerHTML = modalCarrusel;
+document.getElementById('modalGaleriaImagenes').innerHTML = modalGaleriaImagenes;
 document.getElementById('modalCarruselVideos').innerHTML = modalCarruselVideos;
+document.getElementById('modalGaleriaVideos').innerHTML = modalGaleriaVideos;
+document.getElementById('modalGal').innerHTML = modalGal;
 document.getElementById('idFooter').innerHTML = pieDePagina;
 
